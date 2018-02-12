2e94f315ae0032d2501e24c32161cbad358d6567
Kevin Hubert <iamkevinhubert@gmail.com>
2018-02-11 19:29:00 -0600

    changing theme name in functions.php

--- wp-content/themes/theoffcut-child/functions.php
+++ wp-content/themes/theoffcut-child/functions.php
@@ -2,16 +2,16 @@
 /**
  * Child theme functions.
  *
- * @package Atik
- * @since Atik Child 1.0
+ * @package TheOffcut
+ * @since TheOffcut Child 1.0
  */
 
 /**
- * Atik Child Theme
+ * TheOffcut Child Theme
  *
  * Place any custom functionality/code snippets here.
  */
-function atik_child_styles() {
-	wp_enqueue_style( 'atik-parent-style', get_parent_theme_file_uri() . '/style.css', array( 'genericons' ) );
+function TheOffcut_child_styles() {
+	wp_enqueue_style( 'TheOffcut-parent-style', get_parent_theme_file_uri() . '/style.css', array( 'genericons' ) );
 }
-add_action( 'wp_enqueue_scripts', 'atik_child_styles' );
+add_action( 'wp_enqueue_scripts', 'TheOffcut_child_styles' );
