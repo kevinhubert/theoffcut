5867dee718f810576eee22b8ee3f233d69fef494
Kevin Hubert <iamkevinhubert@gmail.com>
2018-02-18 11:19:54 -0600

    some style tweaks

--- wp-content/themes/theoffcut/footer.php
+++ wp-content/themes/theoffcut/footer.php
@@ -32,33 +32,7 @@
 		<div class="site-info">
 			<div class="container">
 				<div class="grid">
-					<div class="copyright grid__col grid__col--1-of-2 grid__col--am">
-						<?php if ( atik_get_thememod_value( 'html-footer-text' ) || is_customize_preview() ) : ?>
-							<div class="site-info-footer">
-								<?php $allowed_tags = atik_allowed_html(); ?>
-								<?php echo wp_kses( atik_get_thememod_value( 'html-footer-text' ), $allowed_tags ); ?>
-							</div>
-						<?php endif; ?>
-
-						<?php if ( false === atik_get_thememod_value( 'bool-hide-credit' ) || is_customize_preview() ) : ?>
-							<div class="site-byline">
-								<?php /* translators: WordPress title */ ?>
-								<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'atik' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'atik' ), 'WordPress' ); ?></a>
-								<span class="sep"> | </span>
-								<?php /* translators: Theme title */ ?>
-								<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'atik' ), 'Atik', '<a href="https://codestag.com" rel="designer">Codestag</a>' ); ?>
-							</div>
-						<?php endif; ?>
-					</div>
-
-					<div class="credit-cards-wrap grid__col grid__col--1-of-2 grid__col--am aligntxtright">
-						<?php if ( atik_get_thememod_value( 'html-footer-card' ) || is_customize_preview() ) : ?>
-							<div class="site-card">
-								<?php $allowed_tags = atik_allowed_html(); ?>
-								<?php echo wp_kses( atik_get_thememod_value( 'html-footer-card' ), $allowed_tags ); ?>
-							</div>
-						<?php endif; ?>
-					</div>
+					
 				</div>
 			</div>
 		</div><!-- .site-info -->
